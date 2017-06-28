179df9e21bd45b415e6bc9a44aa99d982d6d07c0
AZR <azr@AZR-MAC.local>
2017-06-29 03:15:54 +0530

    test db config tweaked

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose');
+var connectionString = 'mongodb://azr:test123@ds141232.mlab.com:41232/todo-app';
+var env = process.env.NODE_ENV || 'development';
 
+if (env === 'developmet') {
+  connectionString = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+  connectionString = 'mongodb://localhost:27017/TodoAppTest';
+}
 mongoose.Promise = global.Promise;
-var connectionString = process.env.PORT? 'mongodb://azr:test123@ds141232.mlab.com:41232/todo-app' : 'mongodb://localhost:27017/TodoApp'
 mongoose.connect(connectionString);
 
 module.exports = {
