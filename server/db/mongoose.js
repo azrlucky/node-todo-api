2b564ad3c69856150919742044ce7732d14dc4d2
AZR <azr@AZR-MAC.local>
2017-06-28 23:53:23 +0530

    mongo db link updated

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var connectionString = process.env.PORT? 'mongodb://azr:test123@ds141232.mlab.com:41232/todo-app' : 'mongodb://localhost:27017/TodoApp'
+mongoose.connect(connectionString);
 
 module.exports = {
   mongoose
